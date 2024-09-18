dfc461025a09056c849fd3c9d153b642d45da8ba
Roman Lorentzen <lo2711ro0518@pusd.local>
2024-09-18 09:48:57 -0700

    Done

---
+++ label.gd
@@ -0,0 +1,11 @@
+extends Label
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.text = "Score: " +str(Global.score)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	self.text = "Score: " +str(Global.score)
